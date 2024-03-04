ee884631e800c94b2188140aafc533c80c4a39f2
ema corra <emanuel.corradini@hotmail.com>
2024-03-04 13:01:35 -0300

    repositorio creado

---
+++ carreras.js
@@ -0,0 +1,20 @@
+/* Array de carreras */
+
+const carreras = [
+    {id: 1, titulo: "Programador FullStack", precio: 1228667, coutas: 33388 },
+    {id: 2, titulo: "Desarrollo UX/UI", precio: 1600000, coutas: 43485 },
+    {id: 3, titulo: "Product Web", precio: 1171333, coutas: 31839 },
+]
+
+console.log(carreras);
+
+// console.log(carreras.find((el) => {
+//     return el.titulo.includes("Programador")
+// }));
+
+
+
+/* Logica para añadir al carrito */
+
+// let usuarioSeleccion = prompt('Seleccione una carrera. \nProgramador FullStack \nDesarrollo UX/UI \nProduct Web');
+
