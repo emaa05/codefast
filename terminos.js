dba529f300051486ca36d42561e337f6a09b658f
ema corra <emanuel.corradini@hotmail.com>
2024-03-18 09:34:50 -0300

    tercera entrega coderhouse

--- terminos.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('showTermsLink').addEventListener('click', function() {
-  document.getElementById('modalContainer').classList.remove('hidden');
-});
-
-document.getElementById('closeTerms').addEventListener('click', function() {
-  document.getElementById('modalContainer').classList.add('hidden');
-});
